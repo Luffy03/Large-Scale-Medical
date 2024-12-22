ccae1410f97d2c1a196abb4ae7ee96a482215ea8
03 <52514670+Luffy03@users.noreply.github.com>
2024-12-22 10:14:33 +0800

    Update train.sh

--- Downstream/train.sh
+++ Downstream/train.sh
@@ -4,8 +4,6 @@ logdir=runs/logs_swin_base_VoComni
 feature_size=48
 data_dir=/data/3Dircadb1_convert/
 cache_dir=/data/cache/3D-IRCADb
-use_ssl_pretrained=False
-use_persistent_dataset=True
 
 mkdir -p $logdir
 
@@ -14,6 +12,6 @@ torchrun --master_port=21503 main.py \
     --feature_size $feature_size \
     --data_dir $data_dir \
     --cache_dir $cache_dir \
-    --use_ssl_pretrained $use_ssl_pretrained \
-    --use_persistent_dataset $use_persistent_dataset \
+    --use_ssl_pretrained \
+    --use_persistent_dataset \
     --logdir $logdir | tee $logdir/$now.txt
